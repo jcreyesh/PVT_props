037ca26f1a94314ef4c6d78bec16014e9983eac0
Jose Carlos Reyes <153877979+jcreyesh@users.noreply.github.com>
2024-09-21 09:16:18 -0600

    Add files via upload

---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using PVT_props
+using Test
+
+@testset "PVT_props.jl" begin
+    
+    # pb_Standing(31, 0.95, 180, 675, 0.2, 0.1, 0.0) == 2174.376877627504
+    # pb_Lasater(31, 0.95, 180, 675, 0.2, 0.1, 0) == 2190.511
+
+end
